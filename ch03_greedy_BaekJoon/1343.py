281297c4d7335df4115892823abd7535e9e1c272
yubeen <dbqls200@gmail.com>
2021-08-02 19:04:51 +0900

    Create 1343.py

---
+++ ch03_greedy_BaekJoon/1343.py
@@ -0,0 +1,14 @@
+# chapter3 - degree
+# 1343번 - 폴리오미노
+
+words = input()
+
+words = words.replace('XXXX', 'AAAA')
+
+words = words.replace('XX', 'BB')
+
+if 'X' in words:
+    print(-1)
+    
+else:
+    print(words)
