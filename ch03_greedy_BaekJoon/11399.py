20f58c2ac09512a85cfebaa98a3fcf5dbb763244
yubeen <dbqls200@gmail.com>
2021-08-04 23:22:32 +0900

    Update 11399.py

--- ch03_greedy_BaekJoon/11399.py
+++ ch03_greedy_BaekJoon/11399.py
@@ -1,3 +1,17 @@
 # chapter3 - greedy
 # 11399번 - ATM
 
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+time = list(map(int, input().split()))
+result = 0
+
+time.sort()
+
+for i in range(n):
+    result += time[i] * (n - i)
+
+print(result)
