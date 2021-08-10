11d99cdc40db1f3feeb51991681f2cb7edfaa7e5
yubeen <dbqls200@gmail.com>
2021-08-10 21:16:08 +0900

    Update 2606.py

--- ch05_DFS-BFS_BeakJoon/2606.py
+++ ch05_DFS-BFS_BeakJoon/2606.py
@@ -1,24 +1,28 @@
 # chapter5 - DFS/BFS
 # 2606번 - 바이러스
 
+
 from sys import stdin
 input = stdin.readline
-
-dic = {}
-
-for i in range(int(input())):
-    dic[i + 1] = set()
-for j in range(int(input())):
-    a, b = map(int, input().split())
-    dic[a].add(b)
-    dic[b].add(a)
 
 def dfs (start, dic):
     for i in dic[start]:
         if i not in visited:
             visited.append(i)
             dfs(i, dic)
+            
+dic = {}
+
+for i in range(int(input())):
+    dic[i + 1] = set()
+    
+for j in range(int(input())):
+    a, b = map(int, input().split())
+    dic[a].add(b)
+    dic[b].add(a)
 
 visited = []
+
 dfs(1, dic)
+
 print(len(visited)-1)
