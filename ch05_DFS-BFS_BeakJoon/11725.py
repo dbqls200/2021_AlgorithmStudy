8caf2227ea038adf4bb53300c5c4738bd322e602
yubeen <dbqls200@gmail.com>
2021-08-12 09:21:24 +0900

    Create 11725.py

---
+++ ch05_DFS-BFS_BeakJoon/11725.py
@@ -0,0 +1,37 @@
+# chapter5 - DFS/BFS
+# 11725번 - 트리의 부모 찾기
+
+from collections import deque
+from sys import stdin
+input = stdin.readline
+
+def bfs(graph, start, visited, parent):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+
+        for i in graph[v]:
+            if not visited[i]:
+                parent[i] = v
+                queue.append(i)
+                visited[i] = True
+
+
+
+    
+n = int(input())
+graph = [[] for _ in range(n + 1)]
+parent = [0] * (n + 1)
+visited = [False] * (n + 1)
+
+for _ in range(n - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+bfs(graph, 1, visited, parent)
+
+for i in range(2, n + 1):
+    print(parent[i])
