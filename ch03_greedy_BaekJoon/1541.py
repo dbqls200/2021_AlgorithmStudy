246bbeb1cf8d4b519f48f5e61b4833a5347e7747
yubeen <dbqls200@gmail.com>
2021-08-14 02:02:49 +0900

    Update 1541.py

--- ch03_greedy_BaekJoon/1541.py
+++ ch03_greedy_BaekJoon/1541.py
@@ -1,13 +1,16 @@
 # chapter3 - greedy
 # 1541번 - 잃어버린 괄호
 
-import sys
 
-input = sys.stdin.readline
-
-line = input()
+array = input().split('-')
 result = 0
 
+for i in array[0].split('+'):
+    result += int(i)
 
 
+for i in array[1:]:
+    for j in i.split('+'):
+        result -= int(j)
+    
 print(result)
