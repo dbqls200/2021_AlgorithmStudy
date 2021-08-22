aa4f8390ef6507bd2fdffd32b5fdb1efdfe925c9
yubeen <dbqls200@gmail.com>
2021-08-22 22:35:07 +0900

    Update 1913.py

--- ch04_implementation_BaekJoon/1913.py
+++ ch04_implementation_BaekJoon/1913.py
@@ -1,10 +1,41 @@
 # chapter4 - implementation
 # 1913번 - 달팽이
 
+import sys
+
+input = sys.stdin.readline
 n = int(input())
-arr = [[] for _ in range(n)]
+m = int(input())
+board = [[0 for _ in range(n)] for _ in range(n)]
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+x = n // 2
+y = n // 2
+num = 1
+length = 0
+
+board[x][y] = num
+
+while True:
+    for i in range(4):
+        for _ in range(length):
+            x += dx[i]
+            y += dy[i]
+            num += 1
+            board[x][y] = num
+
+            if num == m:
+                ans = [x + 1, y + 1]
+
+    if x == y == 0:
+        break
+    x -= 1
+    y -= 1
+    length += 2
 
 for i in range(n):
-    arr[i].append(n ** 2 - i)
+    print(*board[i])
 
-print(arr)
+print(*ans)
